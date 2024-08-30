c42195c5f469b71adb1221ef1e6927a2dbe575a5
navinyadav <34382893+navinyadav@users.noreply.github.com>
2024-08-30 15:59:42 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+data "google-compute-image" "ubuntu"{
+most_recent = true
+project ="ubuntu-os-cloud"
+family = "ubuntu-2204-lts"
+}
+resource "google_compute_instance" "web"{
+name = "web"
+machine_type = "e2-micro"
+
+boot_disk {
+initialize_params {
+image = data.google_compute_image.ubuntu.self_link
+}
+}
+network_interface{
+subnetwork ="default"
+access_config {
+#leave empty for dynamic public IP
+}
+}
+}
